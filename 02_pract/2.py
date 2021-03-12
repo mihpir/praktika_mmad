9a585e1756f5f3e55a3260f8fe0db513ab0a34b6
mihpir <80105904+mihpir@users.noreply.github.com>
2021-03-12 13:09:00 +0300

    Практика 2

---
+++ 02_pract/2.py
@@ -0,0 +1,17 @@
+def absDiff(m1, m2):
+    return [abs(m1[i]-m2[i]) for i in range(len(m1))]
+print('Задача 1:\t', absDiff([4,7,9,8], [1,17,2,8]), '\n')
+
+def fib(n):
+    if n == 1: return [1]
+    elif n == 2: return [1, 1]
+    else:
+        a = fib(n-1)
+        return a + [a[-2] + a[-1]]
+print('Задача 7:\t', fib(10), '\n')
+
+import random
+def minMax(n):
+    a = [round(random.uniform(-1000, 1000), 2) for i in range(n)]
+    return (min(a), max(a))
+print('Задача 10:\t', minMax(10), '\n')
