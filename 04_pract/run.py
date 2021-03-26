e0b8827b3d63efa125ffecab59e68e9da317dd5c
mihpir <80105904+mihpir@users.noreply.github.com>
2021-03-26 09:47:55 +0300

    Add files via upload
    Вернул комментарии

--- 04_pract/run.py
+++ 04_pract/run.py
@@ -1,6 +1,7 @@
 import numpy as np
 from kNN import KNN
 
+# X - обучающее множество
 X = np.array([
   [33, 21, 1],
   [41, 13, 1],
@@ -19,15 +20,18 @@ X = np.array([
   [135, 47, 4],
   [138, 66, 4]]).astype(np.float64)
 
+# ввод роста и веса особи, которую нужно классифицировать
 height = int(input('Введите рост особи: '))
 weight = int(input('Введите вес особи: '))
 
 obj = np.array([height, weight]).astype(np.float64)
-k = 3
 
+# классификация методом k ближайших соседей
+k = 3
 cl = KNN()
 cl.fit(X[:, 0:-1], X[:, -1])
 object_class = cl.predict(obj, k)
 
+# вывод результата классификации
 monkeys = {1: 'lemur', 2: 'schimpanze', 3: 'gorilla', 4: 'orangutan'}
-print(monkeys[object_class])
+print('/nРезультат классификации: ', monkeys[object_class])
